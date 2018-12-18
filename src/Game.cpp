607158e2e41396329909ca5a5b6cb29be7eef29a
Cod3rkane <jcesar.developer@gmail.com>
2018-12-18 21:19:43 +0000

    SDL window and events

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <string>
+#include <SDL2/SDL.h>
+
+int WINDOW_WIDTH = 1280;
+int WINDOW_HEIGHT = 720;
+
+int initSDLFlag(Uint32 flag) {
+  if (SDL_Init(flag) != 0) {
+    std::cout << "SDL_INIT error: " << SDL_GetError() << std::endl;
+
+    return 1;
+  }
+
+  return 0;
+}
 
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  initSDLFlag(SDL_INIT_VIDEO);
+  SDL_Window *window = SDL_CreateWindow("Pandora", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 
+  bool quit = false;
+  SDL_Event e;
+
+  while (!quit) {
+    while (SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
