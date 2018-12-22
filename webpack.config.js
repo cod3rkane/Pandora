4dfd351911f98be1b9d3ea0be27cb1f95bdc30e8
Cod3rkane <jcesar.developer@gmail.com>
2018-12-22 23:09:40 +0000

    devServer

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development', // TODO: must be changed on Production
   entry: './src/index.js',
+  devtool: 'inline-source-map', // TODO: must be changed on Production
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
