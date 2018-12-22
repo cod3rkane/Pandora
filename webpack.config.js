568d695a971d9092decd671c14da04e2a69c6728
Cod3rkane <jcesar.developer@gmail.com>
2018-12-22 23:20:19 +0000

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,44 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development', // TODO: must be changed on Production
   entry: './src/index.js',
   devtool: 'inline-source-map', // TODO: must be changed on Production
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Pandora'
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProvidePlugin({
+      Promise: 'bluebird'
     })
   ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
