8e8874dd1b0b1823c961ee7e3da648c7d4a5d85b
cod3rkane <jcesar.developer@gmail.com>
2019-01-27 17:04:21 +0100

    Apple Fix

--- src/Game.cpp
+++ src/Game.cpp
@@ -29,7 +29,7 @@ int main() {
   }
 
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
