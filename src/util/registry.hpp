72b7bc24cd7f5a79458d50f2a3f0f93771ca6e9f
Cod3rkane <jcesar.developer@gmail.com>
2019-05-01 01:30:16 +0200

    add Registry.hpp

---
+++ src/util/registry.hpp
@@ -0,0 +1,9 @@
+#ifndef UTIL_REGISTRY_HPP
+#define UTIL_REGISTRY_HPP
+
+#include <entt/entity/registry.hpp>
+
+using Registry = entt::DefaultRegistry;
+using Entity = Registry::entity_type;
+
+#endif
