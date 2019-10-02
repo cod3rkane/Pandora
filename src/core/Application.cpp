b775aa6aea21e590ee4ce923e2d1924fc090f3f0
cod3rkane <jcesar.developer@gmail.com>
2019-10-02 19:39:11 +0200

    add DEPTH_BUFFER

--- src/core/Application.cpp
+++ src/core/Application.cpp
@@ -51,7 +51,7 @@ void Application::start() {
         // get frame buffer size - GLFW
 
         glClearColor(0.0f, 0.2f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         game.update();
 
