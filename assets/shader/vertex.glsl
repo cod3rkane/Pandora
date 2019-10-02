4c1b3b4e5526d2a45c60730c041e3af7b47a2749
cod3rkane <jcesar.developer@gmail.com>
2019-10-02 19:37:39 +0200

    First render

--- assets/shader/vertex.glsl
+++ assets/shader/vertex.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 pos;
 
 void main() {
-  gl_Position = vec4(apos.xyz, 1.0);
+  gl_Position = vec4(pos.xyz, 1.0);
 }
--- src/core/Game.h
+++ src/core/Game.h
@@ -5,10 +5,15 @@
 #ifndef CGAME_GAME_H
 #define CGAME_GAME_H
 
+#include <GL/glew.h>
 #include "../util/registry.h"
 
 class Game {
     Registry reg;
+    unsigned int VBO;
+    unsigned int VAO;
+    unsigned int EBO;
+    int shaderProgram;
 public:
     Game();
     void init();
