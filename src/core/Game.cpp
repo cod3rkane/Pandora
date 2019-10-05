36d7c73a32008e445ba70287f64898b47f124291
cod3rkane <jcesar.developer@gmail.com>
2019-10-05 18:17:03 +0200

    clean code

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -3,9 +3,12 @@
 //
 
 #include "Game.h"
+#include <glm/glm.hpp>
 #include "../system/render.h"
+#include "../system/transformations.h"
 #include "../components/Shader.h"
 #include "../components/Render.h"
+#include "../components/Transform.h"
 
 Game::Game() {
 
@@ -20,6 +23,14 @@ void Game::init() {
     const Entity e = reg.create();
 
     reg.assign<Shader>(e, "../assets/shader/vertex-texture2d.glsl", "../assets/shader/fragment-texture2d.glsl", "../assets/textures/wall.jpg");
+    reg.assign<Transform>(
+        e,
+        glm::vec3(0.0f, 0.0f, -3.0f),
+        100,
+        100,
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(1.0f, 1.0f, 1.0f)
+    );
     reg.assign<Renderable>(e);
 
     System::shader(reg);
@@ -34,7 +45,7 @@ void Game::start() {
     // game loop
     // render things
     // Load Systems
-
+    System::transformations(reg);
     System::render(reg);
 }
 
