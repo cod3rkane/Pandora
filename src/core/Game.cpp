8cac5830f90ddf8857f7fa95cf8c1b67baebd053
cod3rkane <jcesar.developer@gmail.com>
2019-10-06 13:15:06 +0200

    clean code and render cube

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -53,24 +53,57 @@ void Game::init() {
     );
     reg.assign<Renderable>(e);
 
+    float verticesCube[] = {
+        // front
+        -1.0f, -1.0f,  1.0f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        1.0f, -1.0f,  1.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        -1.0f,  1.0f,  1.0f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        // back
+        -1.0f, -1.0f, -1.0f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        1.0f, -1.0f, -1.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f,    1.0f, 1.0f,
+        -1.0f,  1.0f, -1.0f,    1.0f, 0.0f, 0.0f,   1.0f, 1.0f
+    };
+    unsigned int indicesCube[] = {
+        // front
+		0, 1, 2,
+		2, 3, 0,
+		// right
+		1, 5, 6,
+		6, 2, 1,
+		// back
+		7, 6, 5,
+		5, 4, 7,
+		// left
+		4, 0, 3,
+		3, 7, 4,
+		// bottom
+		4, 5, 1,
+		1, 0, 4,
+		// top
+		3, 2, 6,
+		6, 7, 3
+    };
+
     const Entity e2 = reg.create();
     reg.assign<Shader>(
         e2,
         "assets/shader/vertex-texture2d.glsl",
         "assets/shader/fragment-texture2d.glsl",
-        "assets/textures/vegetta.jpg",
-        vertices,
-        sizeof(vertices),
-        indices,
-        sizeof(indices)
+        nullptr,
+        verticesCube,
+        sizeof(verticesCube),
+        indicesCube,
+        sizeof(indicesCube)
     );
     reg.assign<Transform>(
         e2,
-        glm::vec3(1.0f, 1.0f, -3.0f),
+        glm::vec3(1.0f, 0.0f, -3.0f),
         100,
         100,
         glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(1.0f, 1.0f, 1.0f)
+        glm::vec3(0.2f, 0.2f, 0.2f)
     );
     reg.assign<Renderable>(e2);
 
--- src/system/render.cpp
+++ src/system/render.cpp
@@ -71,18 +71,6 @@ void System::shader(Registry &reg) {
         glDeleteShader(*vertexShader);
         glDeleteShader(*fragmentShader);
 
-        float vertices[] = {
-            // positions          // colors           // texture coords
-            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,   // top right
-            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
-            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
-        };
-        unsigned int indices[] = {
-            0, 1, 3, // first triangle
-            1, 2, 3  // second triangle
-        };
-
         unsigned int* VAO = &view.get<Renderable>(e).VAO;
         unsigned int* VBO = &view.get<Renderable>(e).VBO;
         unsigned int* EBO = &view.get<Renderable>(e).EBO;
