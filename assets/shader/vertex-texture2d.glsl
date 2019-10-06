4ddca657cfcf0dc5a5fb753e5897247a4fcf7560
cod3rkane <jcesar.developer@gmail.com>
2019-10-06 12:45:20 +0200

    clean code

--- assets/shader/vertex-texture2d.glsl
+++ assets/shader/vertex-texture2d.glsl
@@ -12,7 +12,7 @@ out vec3 ourColor;
 out vec2 TexCoord;
 
 void main() {
-   gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     ourColor = aColor;
     TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
