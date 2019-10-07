2f0741994c1e534b1c83f5b619c56396ab7ab407
cod3rkane <jcesar.developer@gmail.com>
2019-10-07 20:45:05 +0200

    control camera with keyboard

--- src/system/interactions.cpp
+++ src/system/interactions.cpp
@@ -1,11 +1,30 @@
 #include "interactions.h"
 
 #include <entt/entt.hpp>
-#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
 
 #include "../util/Camera.h"
 
-void System::userInputs(Registry &reg, float deltaTime) {
+void System::userInputs(Registry &reg, GLFWwindow* mainWindow, float deltaTime) {
+    if (glfwGetKey(mainWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        // @TODO: change this...
+        glfwSetWindowShouldClose(mainWindow, true);
+    }
+
     Camera* camera = &entt::service_locator<Camera>::ref();
-    camera->Position += camera->Front * 2.5f * deltaTime;
+    float cameraSpeed = 2.5f * deltaTime;
+    // Camera Actions
+    if (glfwGetKey(mainWindow, GLFW_KEY_W) == GLFW_PRESS) {
+        camera->Position += cameraSpeed * camera->Front;
+    }
+    if (glfwGetKey(mainWindow, GLFW_KEY_S) == GLFW_PRESS) {
+        camera->Position -= cameraSpeed * camera->Front;
+    }
+    if (glfwGetKey(mainWindow, GLFW_KEY_A) == GLFW_PRESS) {
+        camera->Position -= glm::normalize(glm::cross(camera->Front, camera->Up)) * cameraSpeed;
+    }
+    if (glfwGetKey(mainWindow, GLFW_KEY_D) == GLFW_PRESS) {
+        camera->Position += glm::normalize(glm::cross(camera->Front, camera->Up)) * cameraSpeed;
+    }
 }
--- src/system/interactions.h
+++ src/system/interactions.h
@@ -1,10 +1,12 @@
 #ifndef CGAME_INTERACTIONS_H
 #define CGAME_INTERACTIONS_H
 
+#include <GLFW/glfw3.h>
+
 #include "../util/registry.h"
 
 namespace System {
-    void userInputs(Registry &reg, float deltaTime);
+    void userInputs(Registry &reg, GLFWwindow* mainWindow, float deltaTime);
 }
 
 #endif // CGAME_INTERACTIONS_H
