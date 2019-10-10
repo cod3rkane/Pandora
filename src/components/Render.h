eb6db40c0f35d1edbdd034fa668513efbb6bf4e6
cod3rkane <jcesar.developer@gmail.com>
2019-10-10 20:43:43 +0200

    new components

--- src/components/Render.h
+++ src/components/Render.h
@@ -5,10 +5,30 @@
 #ifndef CGAME_RENDERABLE_H
 #define CGAME_RENDERABLE_H
 
+#include <iostream>
+#include <glm/ext.hpp>
+
 struct Renderable {
     unsigned int VBO;
     unsigned int VAO;
     unsigned int EBO;
+};
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+struct Texture {
+    unsigned int id;
+    std::string type;
+};
+
+struct Mesh {
+    std::vector<Vertex> vertices;
+    std::vector<unsigned int> indices;
+    std::vector<Texture> textures;
 };
 
 #endif //CGAME_RENDERABLE_H
