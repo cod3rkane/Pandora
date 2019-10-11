c741fa67a81ba8637245fbfd567043c989b5d90b
cod3rkane <jcesar.developer@gmail.com>
2019-10-12 00:31:23 +0200

    clean code

--- src/core/Game.cpp
+++ src/core/Game.cpp
@@ -27,58 +27,6 @@ Game::~Game() {
 void Game::init(GLFWwindow* mainWindow) {
     window = mainWindow;
 
-    Vertex v0 = {glm::vec3(-1.0f, -1.0f,  1.0f), glm::vec3(1.0f, 0.2f, 0.1f), glm::vec2(1.0f, 1.0f)};
-    Vertex v1 = {glm::vec3(1.0f, -1.0f,  1.0f), glm::vec3(1.0f, 0.2f, 0.4f), glm::vec2(1.0f, 1.0f)};
-    Vertex v2 = {glm::vec3(1.0f,  1.0f,  1.0f), glm::vec3(1.0f, 0.2f, 0.3f), glm::vec2(1.0f, 1.0f)};
-    Vertex v3 = {glm::vec3(-1.0f,  1.0f,  1.0f), glm::vec3(1.0f, 0.2f, 0.2f), glm::vec2(1.0f, 1.0f)};
-    Vertex v4 = {glm::vec3(-1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 0.2f, 0.3f), glm::vec2(1.0f, 1.0f)};
-    Vertex v5 = {glm::vec3(1.0f, -1.0f, -1.0f), glm::vec3(1.0f, 0.2f, 0.1f), glm::vec2(1.0f, 1.0f)};
-    Vertex v6 = {glm::vec3(1.0f,  1.0f, -1.0f), glm::vec3(1.0f, 0.2f, 0.2f), glm::vec2(1.0f, 1.0f)};
-    Vertex v7 = {glm::vec3(-1.0f,  1.0f, -1.0f), glm::vec3(1.0f, 0.2f, 0.3f), glm::vec2(1.0f, 1.0f)};
-    std::vector<Vertex> vertices = { v0, v1, v2, v3, v4, v5, v6, v7 };
-    std::vector<unsigned int> indices = {
-         // front
-		0, 1, 2,
-		2, 3, 0,
-		// right
-		1, 5, 6,
-		6, 2, 1,
-		// back
-		7, 6, 5,
-		5, 4, 7,
-		// left
-		4, 0, 3,
-		3, 7, 4,
-		// bottom
-		4, 5, 1,
-		1, 0, 4,
-		// top
-		3, 2, 6,
-		6, 7, 3
-    };
-    std::vector<Texture> textures;
-
-    const Entity e2 = reg.create();
-    reg.assign<Shader>(
-        e2,
-        "assets/shader/vertex.glsl",
-        "assets/shader/fragment.glsl"
-    );
-    reg.assign<Renderable>(e2);
-    reg.assign<Mesh>(
-        e2,
-        vertices,
-        indices
-    );
-    reg.assign<Transform>(
-        e2,
-        glm::vec3(1.0f, 0.0f, -3.0f),
-        100,
-        100,
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.2f, 0.2f, 0.2f)
-    );
-
     createSimpleTerrain(reg);
 
     System::shader(reg);
