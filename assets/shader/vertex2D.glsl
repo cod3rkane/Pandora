211c0a4cf7c265360967503fcde04f8908f5ed2f
cod3rkane <jcesar.developer@gmail.com>
2019-10-27 20:43:24 +0100

    change Vertex2d

--- assets/shader/vertex2D.glsl
+++ assets/shader/vertex2D.glsl
@@ -4,13 +4,11 @@ layout (location = 0) in vec2 vertexPosition;
 layout (location = 1) in vec4 vertexColor;
 layout (location = 2) in mat4 modelMatrix;
 
-uniform vec2 viewport;
 uniform mat4 projection;
-uniform mat4 view;
 
 out vec4 color;
 
 void main() {
-  gl_Position = modelMatrix * vec4(vertexPosition.xy / viewport.xy, 0.0, 1.0);
+  gl_Position = projection * modelMatrix * vec4(vertexPosition, 0.0, 1.0);
   color = vertexColor;
 }
