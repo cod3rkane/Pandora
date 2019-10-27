b991e243a35805e33cde5dcc90d3ace16ca2776a
cod3rkane <jcesar.developer@gmail.com>
2019-10-27 20:43:01 +0100

    Improves UIComponent

--- src/core/ui/UIComponent.h
+++ src/core/ui/UIComponent.h
@@ -20,8 +20,7 @@ namespace UI {
         float width = 50.0f;
         float height = 50.0f;
         glm::vec3 Position;
-        glm::vec3 Scale;
-        glm::vec3 Rotation;
+        GLfloat rotate = 0.0f;
         Entity entity;
         bool isInteractive = false;
     public:
@@ -51,22 +50,6 @@ namespace UI {
 
         glm::vec3 getPosition() {
             return Position;
-        }
-
-        void setScale(glm::vec3 s) {
-            Scale = s;
-        }
-
-        glm::vec3 getScale() {
-            return Scale;
-        }
-
-        void setRotation(glm::vec3 r) {
-            Rotation = r;
-        }
-
-        glm::vec3 getRotation() {
-            return Rotation;
         }
 
         void setVertices(std::vector<Vertex2D> v) {
@@ -101,48 +84,38 @@ namespace UI {
             return entity;
         }
 
+        void setRotate(GLfloat r) {
+            rotate = r;
+        }
+
+        GLfloat getRotate() {
+            return rotate;
+        }
+
         const glm::mat4 getModelMatrix(int windowWidth, int windowHeight) {
-            // resize GUI
-            float xScale = width / windowWidth;
-            float yScale = height / windowHeight;
-            float xOffset = (Position.x / windowWidth) * 2.0f -1.0f;
-            float yOffset = (Position.y / windowHeight) * 2.0f -1.0f;
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, Position);  
 
-            // @TODO: dummy logic, fix that later
-            if (xOffset < 0) {
-                xOffset += xScale;
-            } else {
-                xOffset -= xScale;
-            }
+            model = glm::translate(model, glm::vec3(0.5f * width, 0.5f * height, 0.0f)); 
+            model = glm::rotate(model, rotate, glm::vec3(0.0f, 0.0f, 1.0f)); 
+            model = glm::translate(model, glm::vec3(-0.5f * width, -0.5f * height, 0.0f));
 
-            if (yOffset < 0) {
-                yOffset += yScale;
-            } else {
-                yOffset -= yScale;
-            }
+            model = glm::scale(model, glm::vec3(width, height, 1.0f));
 
-            float inverseYOffset = yOffset * -1.0f;
-
-            return Maths::createTransformationMatrix(
-                Position,
-                glm::vec3(width, height, 0.0f),
-                Rotation
-            );
+            return model;
         }
 
         void setupVertices() {
-            Vertex2D v0 = { glm::vec2(-1.0f, -1.0f) };
-            Vertex2D v1 = { glm::vec2(1.0f, -1.0f) };
-            Vertex2D v2 = { glm::vec2(-1.0f, 1.0f) };
-            Vertex2D v3 = { glm::vec2(-1.0f, 1.0f) };
-            Vertex2D v4 = { glm::vec2(1.0f, -1.0f) };
-            Vertex2D v5 = { glm::vec2(1.0f, 1.0f) };
+            Vertex2D v0 = { glm::vec2(0.0f, 1.0f) };
+            Vertex2D v1 = { glm::vec2(1.0f, 0.0f) };
+            Vertex2D v2 = { glm::vec2(0.0f, 0.0f) };
+            Vertex2D v3 = { glm::vec2(0.0f, 1.0f) };
+            Vertex2D v4 = { glm::vec2(1.0f, 1.0f) };
+            Vertex2D v5 = { glm::vec2(1.0f, 0.0f) };
 
             mesh.vertices = { v0, v1, v2, v3, v4, v5 };
             mesh.color = glm::vec4(0.498039f, 0.596078f, 0.729412f, 1.0f);
             Position = glm::vec3(0.0f, 0.0f, 0.0f);
-            Scale = glm::vec3(1.0f, 1.0f, 1.0f);
-            Rotation = glm::vec3(0.0f, 0.0f, 0.0f);
         }
 
         virtual void update(int windowWidth, int windowHeight, double mouseX, double mouseY, bool isMousePressed) {};
