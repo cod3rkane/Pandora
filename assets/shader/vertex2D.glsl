9bdb0a41271917b45a15ac325fb7229fc98dbd1a
cod3rkane <jcesar.developer@gmail.com>
2019-10-27 10:26:19 +0100

    Changes Shader 2d

--- assets/shader/vertex2D.glsl
+++ assets/shader/vertex2D.glsl
@@ -1,13 +1,13 @@
 #version 410
 
 layout (location = 0) in vec2 vertexPosition;
-layout (location = 1) in vec3 vertexColor;
+layout (location = 1) in vec4 vertexColor;
 layout (location = 2) in mat4 modelMatrix;
 
 uniform vec2 scale;
 // uniform mat4 model;
 
-out vec3 color;
+out vec4 color;
 
 void main() {
   gl_Position = modelMatrix * vec4(vertexPosition * scale, 0.0, 1.0);
