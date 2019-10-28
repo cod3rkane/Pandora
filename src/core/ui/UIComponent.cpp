6a17020275dec95b9308827f174591de570e2e84
Cod3rkane <jcesar.developer@gmail.com>
2019-10-28 22:35:43 +0100

    Change UIComponent

--- src/core/ui/UIComponent.cpp
+++ src/core/ui/UIComponent.cpp
@@ -1,24 +1,5 @@
 #include "UIComponent.h"
 
-void UI::Component::addChildren(ComponentCore& component) {
-    children.push_back(component);
-}
+void UI::Component::update(double mouseX, double mouseY, bool isMousePressed) {
 
-std::vector<UI::ComponentCore> UI::Component::getChildrens() {
-    return children;
-}
-
-void UI::Component::update(int windowWidth, int windowHeight, double mouseX, double mouseY, bool isMousePressed) {
-    if (isInteractive) {
-        std::cout << "x: " << mouseX << " Y: " << mouseY << std::endl;
-        std::cout << "IsPressed: " << isMousePressed << std::endl;
-
-        if (
-            mouseX >= Position.x && mouseX <= (Position.x + width)
-            && mouseY >= Position.y && mouseY <= (Position.y + height)
-        ) {
-            glm::vec3 currentColor = mesh.color;
-            mesh.color = glm::vec4(currentColor, 0.3f);
-        }
-    }
 }
