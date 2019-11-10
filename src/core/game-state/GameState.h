b41f3aecf3a86c8e313bed0a2629396c113b4924
Cod3rkane <jcesar.developer@gmail.com>
2019-11-10 17:27:35 +0100

    add GameState Types

---
+++ src/core/game-state/GameState.h
@@ -0,0 +1,14 @@
+//
+// Created by cod3r on 11/10/19.
+//
+
+#ifndef CGAME_GAMESTATE_H
+#define CGAME_GAMESTATE_H
+
+enum GameStateTypes {
+    RUN,
+    PAUSE,
+    MAIN_MENU,
+};
+
+#endif //CGAME_GAMESTATE_H
