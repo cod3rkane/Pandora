c8c29443796e8bc73458cbaa2dba9dfd5a44b775
Cod3rkane <jcesar.developer@gmail.com>
2019-11-10 22:04:39 +0100

    Button add callback fn

--- src/core/ui/Button.cpp
+++ src/core/ui/Button.cpp
@@ -17,8 +17,15 @@ void UI::Button::update(double mouseX, double mouseY, bool isMousePressed) {
                 ) {
             glm::vec4 currentColor = mesh.color;
             mesh.color = glm::vec4(currentColor.x, currentColor.y, currentColor.z, 0.3f);
+            if (isMousePressed && callback != nullptr) {
+                callback();
+            }
         } else {
             mesh.color = glm::vec4(mesh.color.x, mesh.color.y, mesh.color.z, 1.0f);
         }
     }
+}
+
+void UI::Button::setCallback(const std::function<void(void)>& cb) {
+    callback = cb;
 }
--- src/core/ui/Button.h
+++ src/core/ui/Button.h
@@ -7,10 +7,14 @@
 
 namespace UI {
     class Button : public UI::CoreComponent {
+        std::function<void(void)> callback;
     public:
         Button(): UI::CoreComponent::CoreComponent() {
             UI::CoreComponent::setInteractive(true);
         }
+
+        void setCallback(const std::function<void(void)>& cb) override ;
+
         void update() override;
         void update(double mouseX, double mouseY, bool isMousePressed) override;
     };
